4a1c11e865f7870055798979accbc78e36608a11
Roopa267 <roopalirio17@gmail.com>
2025-09-24 01:14:59 +0530

    Create clincal.py

---
+++ clincal.py
@@ -0,0 +1,6 @@
+predict_tabular_classification_sample(
+    project="569713108976",
+    endpoint_id="1010876696926093312",
+    location="us-central1",
+    instance_dict=[{ "feature_column_a": "value", "feature_column_b": "value" ...}, {...}]
+)
